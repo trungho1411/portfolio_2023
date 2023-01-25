13659f15ca38e9ea9bed0b5a03179391717be3c2
Ho Trung <khanhtrung14111999@gmail.com>
2023-01-25 16:38:09 +0200

    add project cards, css the pills tab and customize project's section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,33 @@
 import { Col, Container, Nav, Row, Tab } from 'react-bootstrap';
+import { ProjectCard } from './ProjectCard';
+import colorSharp2 from '../assets/img/color-sharp2.png';
+import projImg1 from '../assets/img/project-img1.png';
+import projImg2 from '../assets/img/project-img2.png';
+import projImg3 from '../assets/img/project-img3.png';
 
 export const Projects = () => {
+  const projects = [
+    {
+      title: 'Project 1',
+      description: 'This is project 1',
+      imgUrl: projImg1,
+    },
+    {
+      title: 'Project 2',
+      description: 'This is project 2',
+      imgUrl: projImg2,
+    },
+    {
+      title: 'Project 3',
+      description: 'This is project 3',
+      imgUrl: projImg3,
+    },
+    {
+      title: 'Project 4',
+      description: 'This is project 4',
+      imgUrl: projImg1,
+    },
+  ];
   return (
     <section className='project' id='project'>
       <Container>
@@ -9,7 +36,11 @@ export const Projects = () => {
             <h2>Projects</h2>
             <p>Put projects here</p>
             <Tab.Container id='projects-tabs' defaultActiveKey='first'>
-              <Nav variant='pills' defaultActiveKey='/home'>
+              <Nav
+                variant='pills'
+                className='nav-pills mb-5 justify-content-center align-items-center'
+                id='pills-tab'
+              >
                 <Nav.Item>
                   <Nav.Link eventKey='first'>Tab One</Nav.Link>
                 </Nav.Item>
@@ -21,12 +52,21 @@ export const Projects = () => {
                 </Nav.Item>
               </Nav>
               <Tab.Content>
-                <Tab.Pane></Tab.Pane>
+                <Tab.Pane eventKey='first'>
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey='second'>HO Trung</Tab.Pane>
+                <Tab.Pane eventKey='third'>HO Trung</Tab.Pane>
               </Tab.Content>
             </Tab.Container>
           </Col>
         </Row>
       </Container>
+      <img className='background-image-right' src={colorSharp2}></img>
     </section>
   );
 };
